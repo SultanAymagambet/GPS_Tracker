514c12acafb90fe0092bad7619592bd6a64c1f0f
SultanAymagambet <s.aymagambet@gmail.com>
2022-05-20 18:04:29 +0600

    Telephony Manager

--- app/src/main/java/com/example/gps/MainActivity.kt
+++ app/src/main/java/com/example/gps/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.gps
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.telephony.CellInfoGsm
+import android.telephony.CellSignalStrengthGsm
+import android.telephony.TelephonyManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
-import com.chuckerteam.chucker.api.RetentionManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.ktx.database
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private var client = OkHttpClient()
 
-    private val timer = object: CountDownTimer(5000, 1000) {
+    private val timer = object: CountDownTimer(2000, 1000) {
         override fun onTick(millisUntilFinished: Long) {
 
         }
@@ -36,9 +40,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private var array: MutableList<String> = ArrayList()
-    private val database = Firebase.database
-    private val myRef = database.getReference("data")
+//    private var array: MutableList<String> = ArrayList()
+//    private val database = Firebase.database
+//    private val myRef = database.getReference("data")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -90,12 +94,11 @@ class MainActivity : AppCompatActivity() {
 //                val str: String = findViewById<TextView>(R.id.tv_locations).text.toString()
 //                findViewById<TextView>(R.id.tv_locations).text = "$str \n ${it.latitude}, ${it.longitude}"
 
-                array.add("${it.latitude}, ${it.longitude}")
-                myRef.setValue(array)
+//                array.add("${it.latitude}, ${it.longitude}")
+//                myRef.setValue(array)
 
-                run("https://626bb806e5274e6664d09d0c.mockapi.io/v1/add-tracking?${it.latitude}&${it.longitude}")
+                run("http://164.92.153.55:8080/get?${it.latitude + it.longitude}")
 //                run("http://20.115.96.179:8000/v1/writeonly/add-tracking?latitude=${it.latitude}&longitude=${it.longitude}")
-//                run("http://104.248.246.5:8000=")
             }
         }
     }
@@ -111,9 +114,17 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("MissingPermission")
     private fun run(url: String) {
+        val telephonyManager: TelephonyManager =
+            getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val cellInfoGsm: CellInfoGsm = telephonyManager.allCellInfo[0] as CellInfoGsm
+        val cellSignalStrengthGsm: CellSignalStrengthGsm = cellInfoGsm.cellSignalStrength
+        val  dbm = cellSignalStrengthGsm.dbm
 
         val request = Request.Builder()
+            .addHeader("X-DBM", dbm.toString())
+            .addHeader("X-GSMTYPE", networkTypeClass(telephonyManager.networkType))
             .url(url)
             .build()
 
@@ -126,6 +137,35 @@ class MainActivity : AppCompatActivity() {
 
         createOkHttpClient(applicationContext)
 
+    }
+
+    /** Usage: networkTypeClass(telephonyManager.networkType) */
+    fun networkTypeClass(networkType: Int): String {
+        when (networkType) {
+            TelephonyManager.NETWORK_TYPE_GPRS,
+            TelephonyManager.NETWORK_TYPE_EDGE,
+            TelephonyManager.NETWORK_TYPE_CDMA,
+            TelephonyManager.NETWORK_TYPE_1xRTT,
+            TelephonyManager.NETWORK_TYPE_IDEN,
+            TelephonyManager.NETWORK_TYPE_GSM
+            -> return "2G"
+            TelephonyManager.NETWORK_TYPE_UMTS,
+            TelephonyManager.NETWORK_TYPE_EVDO_0,
+            TelephonyManager.NETWORK_TYPE_EVDO_A,
+            TelephonyManager.NETWORK_TYPE_HSDPA,
+            TelephonyManager.NETWORK_TYPE_HSUPA,
+            TelephonyManager.NETWORK_TYPE_HSPA,
+            TelephonyManager.NETWORK_TYPE_EVDO_B,
+            TelephonyManager.NETWORK_TYPE_EHRPD,
+            TelephonyManager.NETWORK_TYPE_HSPAP,
+            TelephonyManager.NETWORK_TYPE_TD_SCDMA
+            -> return "3G"
+            TelephonyManager.NETWORK_TYPE_LTE
+            -> return "4G"
+            TelephonyManager.NETWORK_TYPE_NR
+            -> return "5G"
+            else -> return "Unknown"
+        }
     }
 
     private fun createOkHttpClient(context: Context) {
@@ -141,20 +181,6 @@ class MainActivity : AppCompatActivity() {
             )
             .build()
 
-//        val collector = ChuckerCollector(
-//            context = this,
-//            showNotification = true,
-//            retentionPeriod = RetentionManager.Period.ONE_HOUR
-//        )
-//
-//        val interceptor = ChuckerInterceptor.Builder(context = context)
-//            .collector(collector = collector)
-//            .maxContentLength(
-//                length = 120000L
-//            )
-//            .redactHeaders(emptySet())
-//            .alwaysReadResponseBody(false)
-//            .build()
     }
 
 }
